fadfa6cea6ea07022635a3d6d97f063721a448e4
logeswaridk <9791830975loki@gmail.com>
2024-12-24 14:34:02 +0530

    removed commented lines

--- src/test/java/Ticket_Creation/test.java
+++ src/test/java/Ticket_Creation/test.java
@@ -19,7 +19,7 @@ public class test {
         setup();
         loginPage("https://192.168.4.96:10095/oss", "nocuser2", "nms@123$");
         ttpage();
-        changettcreation();
+        changett();
     }
 
     public static void setup() {
@@ -152,106 +152,41 @@ public class test {
         elementToBeClickable("//div[@id='tt_menu_1_submenu']//li//a[text()='Open Tickets']");
     }
 
-    public static void changettcreation() throws InterruptedException {
+    public static void changett() throws InterruptedException {
+
         elementToBeClickable("//div[@id='TroubleTicketView']//div//div//button[normalize-space(text())='New Ticket']");
         elementToBeClickable("//div[@id='s2id_TypeofTicket']");
         handledropdown("TypeofTicket");
-
-//        WebElement dropdownElement = driver.findElement(By.xpath("//select[@id='ChangerequesterGroup']"));
-//        Select dropdown = new Select(dropdownElement);
-//        String selectedValue = dropdown.getFirstSelectedOption().getText();
-//        if (selectedValue.equals("Main Network Operation Center")) {
-//            System.out.println("Dropdown value matches the username: " + selectedValue);
-//        }
-
         verifySelectedDropdownValue("//select[@id='ChangerequesterGroup']", "Main Network Operation Center");
-
-//        WebElement dropdownElement = driver.findElement(By.xpath("//select[@id='ChangerequesterUser']/option[@value]"));
-//        String ddtext = dropdownElement.getText();
-//        if (ddtext.equals("nocuser1")) {
-//            System.out.println("Dropdown value matches the username: " + ddtext);
-//        }
-
         verifyDropdownValueMatches("//select[@id='ChangerequesterUser']/option[@value]", "nocuser1");
-
-//        WebElement dropdown = driver.findElement(By.xpath("//select[@id='ChangemodeOfRequest']"));
-//        Select dd =  new Select(dropdown);
-//        dd.selectByVisibleText("Email");
-//        String ddtext = dd.getFirstSelectedOption().getText();
-//        System.out.println("Selected option: " + ddtext);
-
         selectByVisibleText("//select[@id='ChangemodeOfRequest']", "Email");
         selectByVisibleText("//select[@id='ChangeLocation']", "ABU TALAT");
-
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-//        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='ReportedTime']")));
-//        element.sendKeys(Keys.ENTER);
-
         enterkeys("//input[@id='ReportedTime']");
-
         selectByVisibleText("//select[@id='ChangeClassification']", "Circuit Change");
         selectByIndex("//select[@id='ChangeSubClassification']", 1);
         selectByIndex("//select[@id='AffectedParties']", 1);
         selectByIndex("//select[@id='AffectedCircuitList']", 1);
-
         enterkeys("//input[@id='startTime1']");
         enterkeys("//input[@id='endTime1']");
         enterkeys("//input[@id='startTime2']");
         enterkeys("//input[@id='endTime2']");
-
-        // check-box
-//        WebElement element = driver.findElement(By.xpath("//div[@class='form-group']//input[@id='ServiceAffecting']"));
-//        Actions act = new Actions(driver);
-//        act.click(element).build().perform();
-//        if(element.isDisplayed()) {
-//            System.out.println(element.getAccessibleName() + ": " + element.isDisplayed());
-//        } else {
-//            System.out.println(element.getAccessibleName() + ": not selected");
-//        }
-
-//        WebElement element = driver.findElement(By.xpath("//input[@id='PowerOn/offRequired']"));
-//        Actions act = new Actions(driver);
-//        act.click(element).build().perform();
-//        act.sendKeys(Keys.ENTER).build().perform();
-//        element.click();
-
         selectByIndex("//select[@id='TriggeredBy']", 1);
         selectByIndex("//select[@id='ImpactRisk']", 1);
         selectByIndex("//select[@id='ChangeImpact']", 1);
-
-//        WebElement element = driver.findElement(By.xpath("//input[@id='ReasonforChange']"));
-//        element.sendKeys("input" + element.getAccessibleName());
-//        System.out.println(element.getText());
-
         sendkeys("//input[@id='ReasonforChange']", "Reason for change");
-
         selectByIndex("//select[@id='Priority']", 1);
         selectByIndex("//select[@id='Urgency']", 1);
-
         sendkeys("//input[@id='subject']", "create change tt");
         sendkeys("//div[@class='note-editable']", "create change tt");
-
         sendkeys("//input[@id='TestPlanAttachments']", "/home/logeswari/Music/Selenium/TT/src/test/resources/files/touch.xls");
         sendkeys("//input[@id='FallbackPlanAttachments']", "/home/logeswari/Music/Selenium/TT/src/test/resources/files/touch.xls");
         sendkeys("//input[@id='SecurityImpactAttachments']", "/home/logeswari/Music/Selenium/TT/src/test/resources/files/touch.xls");
         sendkeys("//input[@id='Attachments']", "/home/logeswari/Music/Selenium/TT/src/test/resources/files/touch.xls");
         sendkeys("//input[@class='ImpactedCircuitAttachment']", "/home/logeswari/Music/Selenium/TT/src/test/resources/files/touch.xls");
-
-//        WebElement ele = driver.findElement(By.xpath("//div[@id='s2id_LinkedTicket']"));
-//        ele.click();
-//        WebElement sk =  driver.findElement(By.xpath("//input[@id='s2id_autogen8_search']"));
-//        sk.sendKeys("EIG-TT");
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        WebElement option = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[@class='select2-results']//li[1]/div")));
-//        option.click();
-
         elementToBeClickable("//div[@id='s2id_LinkedTicket']");
         sendkeys("//input[@id='s2id_autogen8_search']", "EIG-TT");
         elementToBeClickable("//ul[@class='select2-results']//li[1]/div");
-
-        elementToBeClickable("//button[@id='Submit']");
-
+//        elementToBeClickable("//button[@id='Submit']");
     }
-
 
 }
